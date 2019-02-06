f083c72d6c0c40eaf2aaabfddcf5e49aabdf94ae
Calvin Ference <calvin.ference@gmail.com>
2019-02-06 15:52:28 -0500

    Completed Eq typesafe

--- src/main/scala/learnfp/typeclass/Eq.scala
+++ src/main/scala/learnfp/typeclass/Eq.scala
@@ -1,31 +1,25 @@
 package learnfp.typeclass
 
 trait Eq[A] {
-  def eq(lhs:A, rhs:A):Boolean
+  def eq(lhs: A, rhs: A): Boolean
 }
 
 object Eq {
-  def eq[A](lhs:A, rhs:A)(implicit eqt:Eq[A]) = eqt.eq(lhs, rhs)
+  def eq[A](lhs: A, rhs: A)(implicit eqt: Eq[A]): Boolean = eqt.eq(lhs, rhs)
 }
 
-class EqOps[A](lhs:A)(implicit eqt:Eq[A]) {
-  def ====(rhs:A):Boolean = eqt.eq(lhs, rhs)
+class EqOps[A](lhs: A)(implicit eqt: Eq[A]) {
+  def ====(rhs: A): Boolean = eqt.eq(lhs, rhs)
 }
 
 object EqOps {
-  implicit def toEqOps[A](lhs:A)(implicit eqt:Eq[A]) = new EqOps(lhs)
+  implicit def toEqOps[A](lhs: A)(implicit eqt: Eq[A]) = new EqOps(lhs)
 }
 
 object EqInstances {
-  implicit val intEqInstance = new Eq[Int] {
-    override def eq(lhs: Int, rhs: Int): Boolean = ???
-  }
+  implicit val intEqInstance: Eq[Int] = (lhs, rhs) => lhs == rhs
 
-  implicit val stringEqInstance = new Eq[String] {
-    override def eq(lhs: String, rhs: String): Boolean = ???
-  }
+  implicit val stringEqInstance: Eq[String] = (lhs, rhs) => lhs == rhs
 
-  implicit def listEqInstance[A](implicit eqt:Eq[A]) = new Eq[List[A]] {
-    override def eq(lhs: List[A], rhs: List[A]): Boolean = ???
-  }
+  implicit def listEqInstance[A](implicit eqt: Eq[A]): Eq[List[A]] = (lhs, rhs) => (lhs.size == rhs.size) && lhs.zip(rhs).forall({case (l, r) => eqt.eq(l, r)})
 }
