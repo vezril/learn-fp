2a0f3e48f225bed42fcf1f0610a5dc03282c45f3
Calvin Ference <calvin.ference@gmail.com>
2019-02-07 15:22:18 -0500

    Did Id functor

--- src/main/scala/learnfp/functor/Id.scala
+++ src/main/scala/learnfp/functor/Id.scala
@@ -3,7 +3,7 @@ package learnfp.functor
 case class Id[A](value:A)
 
 object IdInstance {
-  implicit val idInstance:Functor[Id] = new Functor[Id] {
-    override def fmap[A, B](a: Id[A])(fx: A => B): Id[B] = ???
+  implicit val idInstance: Functor[Id] = new Functor[Id] {
+    override def fmap[A, B](a: Id[A])(fx: A => B): Id[B] = Id(fx(a.value))
   }
 }
